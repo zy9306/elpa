8bd0967eca88b2f24b32e2f038ddcb6491eb303b
Chtholly <zy99306@outlook.com>
2022-07-22 18:06:26 +0800

    add puni

---
+++ puni-20220702.1336/puni-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from puni.el  -*- no-byte-compile: t -*-
+(define-package "puni" "20220702.1336" "Parentheses Universalistic" '((emacs "26.1")) :commit "e1c4cac260ac0aae5328e9378bec70baa5102e4c" :authors '(("Hao Wang" . "amaikinono@gmail.com")) :maintainer '("Hao Wang" . "amaikinono@gmail.com") :keywords '("convenience" "lisp" "tools") :url "https://github.com/AmaiKinono/puni")
