d0a0e1c400b5c4096c5997f8673548f99666b7cd
Chtholly <zy99306@outlook.com>
2022-08-05 13:09:34 +0800

    add eldoc-box

---
+++ eldoc-box-20220729.844/eldoc-box-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from eldoc-box.el  -*- no-byte-compile: t -*-
+(define-package "eldoc-box" "20220729.844" "Display documentation in childframe" '((emacs "27.1")) :commit "4a57d48115501c68665535877e58694281a50563" :authors '(("Sebastien Chapuis" . "sebastien@chapu.is")) :maintainer '("Yuan Fu" . "casouri@gmail.com") :url "https://github.com/casouri/eldoc-box")
