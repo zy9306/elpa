4bde35b7bcf3c7fe5abf4a820aa8cb3379d32770
Chtholly <zy99306@outlook.com>
2023-08-08 17:03:01 +0800

    add selected-20230219.1328

---
+++ selected-20230219.1328/selected-pkg.el
@@ -0,0 +1,12 @@
+(define-package "selected" "20230219.1328" "Keymap for when region is active" 'nil :commit "1ca6e12f456caa1dc97c3d68597598662eb5de9a" :authors
+  '(("Erik Sjöstrand"))
+  :maintainers
+  '(("Erik Sjöstrand"))
+  :maintainer
+  '("Erik Sjöstrand")
+  :keywords
+  '("convenience")
+  :url "http://github.com/Kungsgeten/selected.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
