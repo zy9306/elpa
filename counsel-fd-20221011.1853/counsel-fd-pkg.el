ac7a7eca45c37293f4a10d25160ea355ac7ffbcd
Chtholly <zy99306@outlook.com>
2023-10-13 17:53:01 +0800

    add counsel-fd

---
+++ counsel-fd-20221011.1853/counsel-fd-pkg.el
@@ -0,0 +1,8 @@
+(define-package "counsel-fd" "20221011.1853" "counsel interface for fd"
+  '((counsel "0.12.0"))
+  :commit "7c1e413e4ce44df2232c19ebe3357ac8ec33cb3b" :keywords
+  '("tools")
+  :url "https://github.com/CsBigDataHub/counsel-fd")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
